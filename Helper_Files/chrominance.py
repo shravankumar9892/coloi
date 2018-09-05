a5d14728173c8936b4c166def03f1ef1d57fcc6c
shravankumar9892 <shravankumarshetty9892@gmail.com>
2018-09-06 01:13:50 +0530

    chromiance -memory error

--- Helper_Files/chrominance.py
+++ Helper_Files/chrominance.py
@@ -4,26 +4,15 @@ import cv2
 # image_array is an array with bgr values of each image, per element.
 image_array = np.load("../../dataset/images.npy")
 
-# Scaling the image between 0 and 1
-#image_array = image_array/255
-
 # converting bgr to luv color space
-image_luv = cv2.cvtColor(image_array, 'CV_RGB2Luv')
-L = []
-u = []
-v = []
-# Seperating L, u and v
-for i in range(25000):
-	a,b,c = image_luv[i]
-	L.append(a)
-	u.append(b)
-	v.append(c)
-L = np.asarray(L)
-u = np.asarray(u)
-v = np.asarray(v)
+Luv = []
 
-np.save("../../dataset/u.npy", u)
-np.save("../../dataset/v.npy", v)
+for i in range(25000):
+	Luv.append(cv2.cvtColor(image_array[i],cv2.COLOR_BGR2LUV))	
+	
+Luv = np.asarray(Luv) # It's showing a memory error in this line :/
+
+np.save("../../dataset/Luv.npy", Luv)
 
 
 
