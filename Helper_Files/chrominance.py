27e95ce8e082cb4007bc2633bd1dcec2e0e18b8c
shravankumar9892 <shravankumarshetty9892@gmail.com>
2018-09-09 16:34:21 +0530

    helper functoins

--- Helper_Files/chrominance.py
+++ Helper_Files/chrominance.py
@@ -1,19 +1,35 @@
 import numpy as np
 import cv2
 
-# image_array is an array with bgr values of each image, per element.
-image_array = np.load("../../dataset/images.npy")
+# Download the dataset from 
+# https://www.kaggle.com/shravankumar9892/image-colorization
 
+def LUV(image, conversion = 1, inter = cv2.INTER_AREA):
+    # Instead of loading images from the array, I'm doing everything again here so that it shouldn't give memory error.
+    img = cv2.imread(image, 1)
+    img = cv2.resize(img, (224,224), interpolation = inter)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    luv = (cv2.cvtColor(img,cv2.COLOR_RGB2LAB)+[0, 128, 128])/[100,255,255]
+    return luv[:,:,0], luv[:,:,1:]
+
+l = []
+ab = []
 # converting bgr to luv color space
-Luv = []
+#for i in range(2):
+#    
+#    l.append(luv[:,:,0])
+#    ab.append(luv[:,:,1:])
+#np.save("../../coloi1/dataset/l.npy", np.asarray(l))
+#np.save("../../coloi1/dataset/ab.npy", np.asarray(ab))        
+#image_array = np.load("../../coloi1/dataset/rgb.npy")     	
 
 for i in range(25000):
-	Luv.append(cv2.cvtColor(image_array[i],cv2.COLOR_BGR2LUV))	
-	
-Luv = np.asarray(Luv) # It's showing a memory error in this line :/
-
-np.save("../../dataset/Luv.npy", Luv)
-
+    file = "../../coloi1/mirflickr25k(1)/mirflickr/im"+str(i+1)+".jpg"
+    m,n = LUV(file)	
+    l.append(m)
+    ab.append(n)	
+np.save("../../coloi1/dataset/l.npy", np.asarray(l))
+np.save("../../coloi1/dataset/ab.npy", np.asarray(ab))
 
 
 
--- Helper_Files/resize.py
+++ Helper_Files/resize.py
@@ -5,19 +5,21 @@ import cv2
 def image_resize(image,conversion = 1, inter = cv2.INTER_AREA):
     # For gray scale: conversion = 0, otherwise let the default condition work.
     img = cv2.imread(image, conversion)
-    resized = cv2.resize(img, (224,224), interpolation = inter)
-    return resized
+    img = cv2.resize(img, (224,224), interpolation = inter)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
 # Create a for loop to iterate over every image.(total == 25k)
 # Save every image(pixels) in a list by appending each one
 # use np.asarry() to convert this list into an array
 # Store this numpy array as .npy file.
+
 image_files = []
 for i in range(25000):
         # Link for mirflickr is given on the project website :)
-	file = "../mirflickr25k(1)/mirflickr/im"+str(i+1)+".jpg"
-	image = np.asarray(image_resize(file, 0))
+	file = "../../coloi1/mirflickr25k(1)/mirflickr/im"+str(i+1)+".jpg"
+	image = np.asarray(image_resize(file))
 	image_files.append(image)
 image_array = np.asarray(image_files)
-np.save("../dataset/gray_scale.npy", image_array)
+np.save("../../coloi1/dataset/rgb.npy", image_array)
 		
